8edbffac4e1d56a6107e840a326dde493a04d5e7
Johan Poirier <johan.poirier@gmail.com>
2015-03-10 17:25:00 +0100

    Upload only pictures & movies to AWS Glacier

--- glacier.js
+++ glacier.js
@@ -1,44 +1,69 @@
 var exec = require('child_process').execSync,
-    fs = require('fs');
+    fs = require('fs'),
+    crypto = require('crypto');
 
-var dir = '/home/pi/hdd/Glacier/2013';
+var dir = '/home/pi/hdd/Photos/2013';
 var doneDir = __dirname + '/done';
+
+var filesToUploadTest = new RegExp("\\.(jpg|jpeg|png|bmp|mkv|mp4|avi|mov|cr2)$", 'i');
 
 var dirFiles;
 
+var getShasum = function (filename) {
+    var shasum = crypto.createHash('sha1');
+    shasum.update(filename);
+    return shasum.digest('hex');
+};
+
 var upload = function (filename) {
+    var relFilename = '2013' + filename.substring(dir.length - 1).replace(/ /g, '\\ ');
+
     var cmdArgs = ['mtglacier', 'upload-file',
         '--config /home/pi/.glacier.cfg',
         '--vault Photos',
         '--journal /home/pi/glacier/journal.log',
-        '--filename /home/pi/hdd/Glacier/2013/' + filename.replace(/ /g, '\\ '),
-        '--set-rel-filename 2013/' + filename.replace(/ /g, '\\ ')];
+        '--filename ' + filename.replace(/ /g, '\\ '),
+        '--set-rel-filename ' + relFilename,
+        '--partsize 8'];
 
-    if (fs.existsSync(doneDir + '/' + filename)) {
+    if (fs.existsSync(doneDir + '/' + getShasum(relFilename))) {
         console.log(filename + " already uploaded");
         nextUpload();
         return;
     }
 
     console.log(cmdArgs.join(' '));
-    var glacier = exec(cmdArgs.join(" "), { stdio: 'pipe' });
 
-    glacier.stdout.setEncoding('utf8');
-    glacier.stdout.on('data', function (data) {
-        console.log(data);
-    });
-
-
-    glacier.stderr.setEncoding('utf8');
-    glacier.stderr.on('data', function (data) {
-        console.error(data)
-    });
-
-    glacier.on('close', function () {
-        exec('touch "' + doneDir + '/' + filename + '"');
+    try {
+        console.log(exec(cmdArgs.join(" "), { stdio: 'pipe' }).toString('ascii'));
+        exec('touch "' + doneDir + '/' + getShasum(relFilename) + '"');
+    } catch (e) {
+        console.error("Error during upload of " + relFilename, e.stderr.toString());
+    } finally {
         nextUpload();
-    });
+    }
 };
+
+var mockUpload = function (filename) {
+    var relFilename = '2013/' + filename.substring(dir.length + 1);
+    var cmdArgs = ['mtglacier', 'upload-file',
+        '--config /home/pi/.glacier.cfg',
+        '--vault Photos',
+        '--journal /home/pi/glacier/journal.log',
+        '--filename ' + filename.replace(/ /g, '\\ '),
+        '--set-rel-filename ' + relFilename.replace(/ /g, '\\ ')];
+
+    if (fs.existsSync(doneDir + '/' + getShasum(relFilename))) {
+        console.log(relFilename + " already uploaded");
+        nextUpload();        return;
+    }
+
+    console.log(cmdArgs.join(' '));
+    exec('touch "' + doneDir + '/' + getShasum(relFilename) + '"');
+    nextUpload();
+};
+
+//upload = mockUpload;
 
 var nextUpload = function () {
     if (dirFiles.length > 0) {
@@ -46,8 +71,22 @@ var nextUpload = function () {
     }
 };
 
+var readFiles = function (dir) {
+    var files = fs.readdirSync(dir),
+        filesToUpload = [];
+
+    files.forEach(function (filename) {
+        var fullpath = dir + '/' + filename;
+        var file = fs.statSync(fullpath);
+        if (file.isDirectory()) {
+            filesToUpload = filesToUpload.concat(readFiles(fullpath));
+        } else if (filesToUploadTest.test(fullpath)) {
+            filesToUpload.push(fullpath);
+        }
+    });
+    return filesToUpload;
+};
+
 // Read files
-fs.readdir(dir, function (err, files) {
-    dirFiles = files;
-    upload(dirFiles.shift());
-});
+dirFiles = readFiles(dir);
+upload(dirFiles.shift());
